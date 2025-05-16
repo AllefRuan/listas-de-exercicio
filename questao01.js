a83977e69d4f7b0fd645c119f9bb0384040e43a2
AllefRuan <rescola847@gmail.com>
2025-05-16 16:49:52 +0000

    atualizacao 2

---
+++ questao01.js
@@ -0,0 +1,10 @@
+function somarNumeros(num1, num2) {
+  return num1 + num2;
+}
+
+// Exemplo de uso:
+let resultado = somarNumeros(5, 7);
+console.log("A soma é:", resultado); // Saída soma
+
+
+
---
+++ questao02.js
@@ -0,0 +1,7 @@
+function dobro(numero) {
+  return numero * 2;
+}
+
+
+let resultado = dobro(4);
+console.log("O dobro é:", resultado); 
